2f5950f684ab1d5c34dc83641b7c399e49971846
Andraš <andras.tsitskan@gmail.com>
2022-06-28 15:52:18 +0300

    Add initial fixed grid functionality
    Add 16x16 grid with temporary styling, including colour change on
    hover.

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const grid = document.querySelector(".grid");
+
+function createGridItems(squaresPerSide) {
+  for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+    let newGridItem = document.createElement("div");
+    newGridItem.classList.add("grid-item");
+    grid.appendChild(newGridItem);
+  }
+}
+
+createGridItems(16);
+
+const gridItems = document.querySelectorAll(".grid-item");
+
+for (const item of gridItems) {
+  item.addEventListener("mouseenter", changeColor);
+}
+
+function changeColor(e) {
+  e.target.classList.add("grid-item-hovered");
+}
+
+// function getGridItemWidth(){
+//     document.body.clientWidth
+// }
